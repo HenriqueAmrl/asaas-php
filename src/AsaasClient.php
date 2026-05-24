f99b5bfcf829faa9bd4a443a5c482be38d901860
Henrique Amaral <henriqueamaralreis@gmail.com>
2026-05-24 13:28:58 -0300

    refactor: remove getHttpClient accessor from AsaasClient
    Property is write-only until resource accessors are added. Suppress
    PHPStan property.onlyWritten inline rather than exposing internal
    HTTP client as public API.

--- src/AsaasClient.php
+++ src/AsaasClient.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace HenriqueAmrl\AsaasPhp;
 
-use HenriqueAmrl\AsaasPhp\Environment;
 use HenriqueAmrl\AsaasPhp\Http\HttpClient;
 use Psr\Http\Client\ClientInterface;
 
 final class AsaasClient
 {
-    private readonly HttpClient $httpClient;
+    private readonly HttpClient $httpClient; // @phpstan-ignore property.onlyWritten
 
     public function __construct(
         string $apiKey,
@@ -23,20 +22,4 @@ final class AsaasClient
             client: $httpClient,
         );
     }
-
-    /**
-     * Returns the internal HttpClient for use by resource accessor methods.
-     *
-     * @internal Used by resource accessor methods added in later work.
-     */
-    public function getHttpClient(): HttpClient
-    {
-        return $this->httpClient;
-    }
-
-    // Future resource accessors added here in later work:
-    // public function customers(): CustomerResource
-    // public function charges(): ChargeResource
-    // public function subscriptions(): SubscriptionResource
-    // public function pix(): PixResource
 }
